7887f44ba82c405a0c07a872e1dc56bef1a86df1
nickshan <157492329+nickshan01@users.noreply.github.com>
2024-01-24 20:25:43 +0545

    Create 01_variable.js
    var,let,constant

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 567890765
+let accountEmail = "nidhsn@gmsil.com"
+var accountPassword = "60833649"
+accountCity = "kathmandu"
+let accountState;
+
+let accountIe = 74 //not allowed or changed 
+accountEmail = "hero@gmail.com"
+accountPassword = "840"
+accountCity = "jumla"
+
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState, accountIe])
