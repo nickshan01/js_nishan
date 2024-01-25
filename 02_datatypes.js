17e02080bfec8c59c76afe1d1cfcbfa16d7661a8
nickshan <157492329+nickshan01@users.noreply.github.com>
2024-01-25 19:36:44 +0545

    Create 02_datatypes.js
    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,26 @@
+ "use strict" //treat all js code as newer version or js
+
+//alert(3 + 3 ) // e are using node js, not browser 
+
+console.log(3 + 3);
+
+console.log("nishan")     //code should readable 
+
+let name =  "nishan"
+let age = 14 
+let isLoggedin = true
+
+/*
+
+data types are given below:
+number = 2 to 53 
+bigint 
+string = "" 
+boolean = true  or false 
+null = standalone value 
+undefined 
+symbol = unique 
+
+*/
+
+console.log(typeof age )
