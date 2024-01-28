2fc635cbe1b9290ff757407d0aec625b073dc6db
nickshan <157492329+nickshan01@users.noreply.github.com>
2024-01-28 18:42:41 +0545

    Update 03_conversionofdatatypes.js
    conversion of data types

--- 01_basics/03_conversionofdatatypes.js
+++ 01_basics/03_conversionofdatatypes.js
@@ -34,3 +34,46 @@ let dipeshIsGay = true;
 let booleanInInt = BigInt(dipeshIsGay);
 console.log(booleanInInt);
 console.log(typeof booleanInInt);
+
+//        conversion 
+
+
+let value = 654 
+let negValue =-value
+console.log(negValue);
+
+
+console.log(2 +2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+let str1 = "hello"
+let str2 = "world"
+let str3 =  str1 + str2
+console.log(str3);
+
+let str4 = "hi"
+let str5 = "maya"
+console.log(str4 + str5);
+
+console.log("1"+2);
+console.log(1 +"2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+
+let myAge = 15
+myAge++;
+++myAge;
+console.log(myAge);
+
+console.log(+true);
+console.log(+"");
+
+
+let a , b , c 
+a = b = c = 87436+784
+console.log(a,b,c);
