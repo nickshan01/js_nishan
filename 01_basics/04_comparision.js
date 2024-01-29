75bf4b76e88b9d1cd08798014d8006e49bb92359
nickshan <157492329+nickshan01@users.noreply.github.com>
2024-01-29 18:18:46 +0545

    Create 04_comparision.js
    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+//it compaare  two values
+console.log(100 > 55); 
+console.log(100 >= 55); 
+console.log(100 < 55);
+console.log(100 != 55);
+console.log(100 == 55);
+
+
+console.log("100" > 55);
+console.log("100" == "100"); //it returns true because bohtare  string so , it compare character by character 
+
+console.log(null > 0);   
+console.log(null < 0);
+console.log(null >= 0);
+
+/*---------------------------undefined------------------------------------*/
+console.log(undefined > 0);   
+console.log(undefined < 0);
+console.log(undefined >= 0);
+
+
+//===
+
+console.log(null === 0);
+console.log("100" >= 100); //strict check 
