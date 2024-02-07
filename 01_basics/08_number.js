e06575e2edf6b88e7e274d4ac4138fc811ba74a5
nickshan <157492329+nickshan01@users.noreply.github.com>
2024-02-07 18:05:14 +0545

    Create 08_number.js
    numbers

---
+++ 01_basics/08_number.js
@@ -0,0 +1,15 @@
+const value = 549
+console.log(value);
+
+
+const  number = new Number(78547);
+console.log(number);
+
+console.log(number.toFixed(2));
+
+const thousands = 1000000000000000000
+
+console.log(thousands.toLocaleString('en-IN'));
+
+const otherNumber = 63.3455
+console.log(otherNumber.toPrecision(3));
