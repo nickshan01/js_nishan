d04be3a0482611fa36789b86fc5744d84078fa12
nickshan <157492329+nickshan01@users.noreply.github.com>
2024-02-25 18:26:32 +0545

    Create 15_objectsDE-Structures.js
    objects DE-Structures

---
+++ 02basics/15_objectsDE-Structures.js
@@ -0,0 +1,31 @@
+const gamer = {
+    gameName  : "FREE FIRE",
+    gamerName : "Nick Shan",
+    gamerUid : 837283786,
+    gamerSalary : 99999999999
+}
+
+// gamer.gameName = "Pubg Mobile"
+
+const {gamerSalary : Salary} = gamer
+console.log(Salary) // 999999999
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++JSON++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// {
+//     "name" : "nick Shan",
+//     "age" : 15
+//     "job" : "coding"
+// }
+
+
+// +++++++++++++++++++++++++++++Array Json++++++++++++++++++++++++++++++++++
+
+[
+   {},
+   {},
+   {},
+   {}
+]
+
+console.log(typeof NaN);
