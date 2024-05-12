184917ce785faf77c5b520696cdf9b7eb472d4c9
nickshan <157492329+nickshan01@users.noreply.github.com>
2024-05-12 11:59:09 +0545

    Create 19_ScopelevelandMinihosting.js
    Scope level and Mini hosting

---
+++ 03_basics/19_ScopelevelandMinihosting.js
@@ -0,0 +1,39 @@
+function one() {
+    username = "Nick Shan"
+    
+    function two(){
+        website= "github"
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+
+}
+one();
+
+
+if (true) {
+   const characterName = "Motu"
+
+    if (characterName === "Motu") {
+      const  characterRace = "Human"
+        console.log(`His name is ${characterName}  His race  is ${characterRace}`);
+        console.log(characterName + characterRace);
+    }
+    // console.log(characterRace);
+}
+// console.log(characterName);
+
+
+function addone(num) {
+    return num + 1
+
+}
+// addone(49);
+console.log(addone(99));
+
+const addhundred = function(value) {
+    return value + 100
+}
+// addhundred(50);
+console.log(addhundred(50));
