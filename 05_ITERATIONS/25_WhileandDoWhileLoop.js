e10e3f548144fd5d782f5a87bbd679870dfe674c
nickshan <157492329+nickshan01@users.noreply.github.com>
2024-05-12 12:12:37 +0545

    Create 25_WhileandDoWhileLoop.js
    While and Do While Loop

---
+++ 05_ITERATIONS/25_WhileandDoWhileLoop.js
@@ -0,0 +1,19 @@
+let index = 0;
+while (index <= 10) {
+    console.log(`The value of index is ${index}`);
+    index++
+}
+
+let myArray = ["apple", "banana", "orange"]
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+score = 1;
+
+do {
+    console.log(`value of score is ${score}`);
+    score++
+} while (score <= 10);
